9fcaa6cbceea47065edf81abf6d5f8f99bba6def
harrish159 <jkhr2005@gmail.com>
2026-03-06 14:58:11 +0530

    Adding Trainings is Done for Faculty

--- frontend/src/Components/hod/training_hod.jsx
+++ frontend/src/Components/hod/training_hod.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import {
   GraduationCap,
@@ -15,6 +16,7 @@ import {
   ChevronDown,
   ChevronUp,
 } from "lucide-react";
+
 
 const TrainingHod = () => {
   const [programs, setPrograms] = useState([]);
