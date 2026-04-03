46c0aaf90d57224ad5de58490db55921eba00e43
harrish159 <jkhr2005@gmail.com>
2026-04-03 18:27:18 +0530

    CORS updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "http://localhost:3000",
+    "https://skill-gap-analysis-harrish.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
